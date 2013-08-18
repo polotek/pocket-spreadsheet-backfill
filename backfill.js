e40d70c8657461911ae6b0dd20d8f19bdffa71fc
Marco Rogers <marco.rogers@gmail.com>
2013-08-17 20:29:47 -0700

    First mostly successful pass
    This is the most straight forward approach. Serial execution using
    callbacks all the way down. There is some ugly stuff here. The pocket results
    need to be sorted inline. The spreadsheet rows are added individually and
    uses an async counter. The row adds are not serialized and can potentially
    be added out of order due to race conditions.

--- backfill.js
+++ backfill.js
@@ -8,24 +8,47 @@ var pConfig = require('./pocket_config.json')
 var p = new Pocket(pConfig.consumer_key, pConfig.access_token)
   , sheet = new GoogleSpreadsheet(gConfig.spreadsheet_key);
 
-p.get({
-  state: 'archive'
-  , sort: 'newest'
-  , detailType: 'complete'
-  , count: 10
-}, function(err, data) {
-  if(err) { throw err; }
-
-  console.log(data);
-});
-
-// set auth to be able to edit/add/delete
 sheet.setAuth(gConfig.username, gConfig.password, function(err){
   if(err) { throw err; }
 
   sheet.getInfo(function(err, sheetInfo) {
     if(err) { throw err; }
 
-    console.log(sheetInfo);
+    p.get({
+      state: 'archive'
+      , sort: 'newest'
+      , detailType: 'complete'
+      , count: 10
+    }, function(err, results) {
+      if(err) { throw err; }
+
+      var archiveList = []
+        , key
+        , item
+        , sortId;
+      for(var key in results.list) {
+        item = results.list[key];
+        sortId = parseInt(item.sort_id, 10);
+        archiveList[sortId] = item;
+      }
+
+      var date = 'August 17, 2013 at 8:04AM' // because pocket doesn't supply actual date info
+        , ctr = archiveList.length;
+      archiveList.forEach(function(item) {
+        sheet.addRow(1, {
+          date: date
+          , article: item.resolved_title
+          , tags: ''
+          , url: item.resolved_url
+          , excerpt: item.excerpt || ''
+        }, function(err) {
+          if(err) { throw err; }
+
+          if(--ctr === 0) {
+            console.log('successfully completed');
+          }
+        });
+      });
+    });
   });
 });
