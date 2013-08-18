4af7d81b99cf382ee505c924ebfd2c6a8bbfa213
Marco Rogers <marco.rogers@gmail.com>
2013-08-17 21:02:47 -0700

    A little cleanup and adding a few comments

--- backfill.js
+++ backfill.js
@@ -1,20 +1,25 @@
-var cs = require('coffee-script')
+var cs = require('coffee-script') // Needed for node-pocket. grrr.
   , Pocket = require('node-pocket')
   , GoogleSpreadsheet = require('google-spreadsheet');
 
+// Setup the Pocket API
 var pConfig = require('./pocket_config.json')
-, gConfig = require('./google_config.json')
+  , pocketClient = new Pocket(pConfig.consumer_key, pConfig.access_token);
 
-var p = new Pocket(pConfig.consumer_key, pConfig.access_token)
+// Setup the Google API
+var gConfig = require('./google_config.json')
   , sheet = new GoogleSpreadsheet(gConfig.spreadsheet_key);
 
+// The Google client needs to be authorized explicitly
 sheet.setAuth(gConfig.username, gConfig.password, function(err){
   if(err) { throw err; }
 
+  // I don't know if I need to get the sheet info
   sheet.getInfo(function(err, sheetInfo) {
     if(err) { throw err; }
 
-    p.get({
+    // Get the pocket archive list
+    pocketClient.get({
       state: 'archive'
       , sort: 'newest'
       , detailType: 'complete'
@@ -22,6 +27,8 @@ sheet.setAuth(gConfig.username, gConfig.password, function(err){
     }, function(err, results) {
       if(err) { throw err; }
 
+      // The archive list is an object with sort ids rather than
+      // an array. Needs to explicitly sorted.
       var archiveList = []
         , key
         , item
@@ -34,6 +41,7 @@ sheet.setAuth(gConfig.username, gConfig.password, function(err){
 
       var date = 'August 17, 2013 at 8:04AM' // because pocket doesn't supply actual date info
         , ctr = archiveList.length;
+      // Add each pocket item as a spreadsheet row
       archiveList.forEach(function(item) {
         sheet.addRow(1, {
           date: date
@@ -44,6 +52,7 @@ sheet.setAuth(gConfig.username, gConfig.password, function(err){
         }, function(err) {
           if(err) { throw err; }
 
+          // Log when we're done
           if(--ctr === 0) {
             console.log('successfully completed');
           }
