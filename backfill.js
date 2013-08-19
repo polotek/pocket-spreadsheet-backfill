76768e1c6ccc95bf47e751dfa1a2205c1c8c86b4
Marco Rogers <marco.rogers@gmail.com>
2013-08-18 17:05:08 -0700

    First pass at promises implementation. Not working
    This is an attempt at replicating the naive approach, only using promises.
    The Q library is used to wrap the methods from the api modules and promises
    are returned from async calls throughout. It was much easier to deal with
    calls that I wanted to happen in serial or in  parallel.
    
    However the script isn't working. There is no output from running the script,
    and the test file fails an assertion. Early attempts to debug have been
    frustrating. There are no errors thrown, and I haven't found the right places
    to place log lines. I'm sure this is just my own lack of familiarity though.

--- backfill.js
+++ backfill.js
@@ -1,31 +1,61 @@
 var cs = require('coffee-script')
+  , Q = require('q')
   , Pocket = require('node-pocket')
   , GoogleSpreadsheet = require('google-spreadsheet');
 
 var pConfig = require('./pocket_config.json')
-, gConfig = require('./google_config.json')
+  , p = new Pocket(pConfig.consumer_key, pConfig.access_token)
 
-var p = new Pocket(pConfig.consumer_key, pConfig.access_token)
+var gConfig = require('./google_config.json')
   , sheet = new GoogleSpreadsheet(gConfig.spreadsheet_key);
 
-p.get({
-  state: 'archive'
-  , sort: 'newest'
-  , detailType: 'complete'
-  , count: 10
-}, function(err, data) {
-  if(err) { throw err; }
+var pGet = Q.nbind(p.get, p)
+  , setAuth = Q.nbind(sheet.setAuth, sheet)
+  , getInfo = Q.nbind(sheet.getInfo, sheet)
+  , addRow = Q.nbind(sheet.addRow, sheet);
 
-  console.log(data);
-});
+setAuth(gConfig.username, gConfig.password)
+  .then(function(){
+    var sInfo = getInfo()
+      , pResults = pGet({
+        state: 'archive'
+        , sort: 'newest'
+        , detailType: 'complete'
+        , count: 10
+      });
+    return Q.all(sInfo, pResults);
+  })
+  .then(function(sheetInfo, results) {
+    // The archive list is an object with sort ids rather than
+    // an array. Needs to explicitly sorted.
+    var archiveList = []
+      , key
+      , item
+      , sortId;
+    for(var key in results.list) {
+      item = results.list[key];
+      sortId = parseInt(item.sort_id, 10);
+      archiveList[sortId] = item;
+    }
 
-// set auth to be able to edit/add/delete
-sheet.setAuth(gConfig.username, gConfig.password, function(err){
-  if(err) { throw err; }
+    var date = 'August 17, 2013 at 8:04AM' // because pocket doesn't supply actual date info
+      , ctr = archiveList.length;
+    // Add each pocket item as a spreadsheet row
+    var addResults = archiveList.map(function(item) {
+      return addRow(1, {
+        date: date
+        , article: item.resolved_title
+        , tags: ''
+        , url: item.resolved_url
+        , excerpt: item.excerpt || ''
+      });
+    });
 
-  sheet.getInfo(function(err, sheetInfo) {
-    if(err) { throw err; }
-
-    console.log(sheetInfo);
+    return Q.all(addResults);
+  })
+  .fail(function(err) {
+    throw err;
+  })
+  .then(function() {
+    console.log('successfully completed');
   });
-});
